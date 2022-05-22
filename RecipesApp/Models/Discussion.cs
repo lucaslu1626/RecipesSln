5790612bc516895198c6213baa5f23a282deb036
Matthew Caldwell <matt.c.caldwell@outlook.com>
2022-05-22 03:02:29 -0700

    1. added UserName to User class 2. changed DiscussionUser to a string to reflect the UserName who made the post instead of their ID
    Need to add
    ---------
    We need some way to link a discussion to the recipe

--- RecipesApp/Models/Discussion.cs
+++ RecipesApp/Models/Discussion.cs
@@ -3,7 +3,7 @@
     public class Discussion
     {
         public long? DiscussionID { get; set; }
-        public long? DiscussionUser { get; set; } //discussion user type = user id?
+        public string DiscussionUser { get; set; } = String.Empty; //discussion user type = user id?
         public string DiscussionPost { get; set; } = String.Empty;
         public string DiscussionDate { get; set; } = String.Empty;
     }
--- RecipesApp/Models/User.cs
+++ RecipesApp/Models/User.cs
@@ -4,5 +4,6 @@
     {
         public long? UserID { get; set; }
         public string UserPW { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
     }
 }
