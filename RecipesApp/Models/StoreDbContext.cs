cf74ff809380605a48a2ad07bd4886206b424fda
Matthew Caldwell <matt.c.caldwell@outlook.com>
2022-05-25 19:05:23 -0700

    discussion relationship with recipe

--- RecipesApp/Models/StoreDbContext.cs
+++ RecipesApp/Models/StoreDbContext.cs
@@ -22,9 +22,10 @@ namespace RecipesApp.Models
                 .WithMany(b => b.Discussions)
                 .HasForeignKey(p => p.DiscussionUser)
                 .HasPrincipalKey(b => b.UserName);
-            //modelBuilder.Entity<Discussion>()
-            //    .HasOne(p => p.Recipe)
-            //    .WithMany(b => b.Discussions)
+
+            modelBuilder.Entity<Discussion>()
+                .HasOne(p => p.Recipe)
+                .WithMany(b => b.Discussions);
             //    .HasForeignKey(p => p.DiscussionRecipe)
             //    .HasPrincipalKey(b => b.RecipeCreator);
         }
