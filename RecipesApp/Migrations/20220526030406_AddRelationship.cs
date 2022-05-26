4cb9fd334ccbecc1fbc051dd2ee4ca726d25a590
Lucas Lu <lucaslu1626@gmail.com>
2022-05-25 20:18:20 -0700

    migrations

---
+++ RecipesApp/Migrations/20220526030406_AddRelationship.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecipesApp.Migrations
+{
+    public partial class AddRelationship : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "RecipeID",
+                table: "Discussions",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Discussions_RecipeID",
+                table: "Discussions",
+                column: "RecipeID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Discussions_Recipes_RecipeID",
+                table: "Discussions",
+                column: "RecipeID",
+                principalTable: "Recipes",
+                principalColumn: "RecipeID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Discussions_Recipes_RecipeID",
+                table: "Discussions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Discussions_RecipeID",
+                table: "Discussions");
+
+            migrationBuilder.DropColumn(
+                name: "RecipeID",
+                table: "Discussions");
+        }
+    }
+}
