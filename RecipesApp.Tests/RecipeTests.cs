58d9f610d4ea56989ba9a99e17d40fcfc4d9983a
Matthew Caldwell <matt.c.caldwell@outlook.com>
2022-06-10 02:13:34 -0700

    created RecipeTests to perform a simple test for the Recipe Class

---
+++ RecipesApp.Tests/RecipeTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RecipesApp.Controllers;
+using RecipesApp.Models;
+using Xunit;
+
+namespace RecipesApp.Tests
+{
+    public class RecipeTests
+    {
+        [Fact]
+        public void ChangeRecipeDetails()
+        {
+            // Arrange
+            var recipe = new Recipe();
+            recipe.RecipeName = "Golden Apple Pie";
+            recipe.RecipeCategory = "American";
+
+            // Act
+            recipe.RecipeName = "Pulled Pork Tacos";
+            // Assert
+            Assert.Equal("Pulled Pork Tacos", recipe.RecipeName);
+            Assert.Equal("American", recipe.RecipeCategory);
+
+        }
+    }
+}
